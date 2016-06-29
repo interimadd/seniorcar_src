111038d1e7998c4473b9398a30e08224c1de9bfa
interimadd <interimadd@gmail.com>
2016-06-29 17:50:51 +0900

    不要なファイルの削除

--- point_cloud_converter-hydro-devel/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ros01/catkin_ws/src/point_cloud_converter-hydro-devel
-# Build directory: /home/ros01/catkin_ws/src/point_cloud_converter-hydro-devel/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(gtest)
--- point_cloud_converter-hydro-devel/build/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/gtest
-# Build directory: /home/ros01/catkin_ws/src/point_cloud_converter-hydro-devel/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
